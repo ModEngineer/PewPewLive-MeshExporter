9165a27d64f3f5cf5c174e16c9561f4f9340f937
ModEngineer <43882205+ModEngineer@users.noreply.github.com>
2021-07-21 09:24:39 -0500

    Fixed a lot of bugs in exportmesh.py
    1. Fixed an issue where properties failed to register because they had hard_min instead of min
    2. Fixed an indexing issue with includedVertices caused by the list not being sorted by vertex index
    3. Fixed an issue where vertex colors were being processed but not saved into a dictionary
    Version number changed from 0.1.6 to 0.1.7

--- ppl_meshexport_addon/__init__.py
+++ ppl_meshexport_addon/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "author": "ModEngineer",
     "blender": (2, 90, 0),
     "description": "A mesh exporter for PewPew Live",
-    "version": (0, 1, 6),
+    "version": (0, 1, 7),
     "tracker_url":
     "https://www.github.com/ModEngineer/PewPewLive-MeshExporter/issues",
     "category": "Import-Export"
