d39bf7675cb56ca566f375efa1c6fe073ec9475f
ModEngineer <43882205+ModEngineer@users.noreply.github.com>
2021-07-22 19:28:16 -0500

    Update testblender.py

--- .github/workflows/testblender.py
+++ .github/workflows/testblender.py
@@ -34,7 +34,7 @@ try:
         edge.seam = True
     bm.to_mesh(currentobj.data)
     bpy.ops.object.mode_set(mode='EDIT')
-    bpy.data.window_managers["WinMan"].pewpew_temporaryproperties.editmode_vertex_color = (0.5, 0.5, 1.0, 0.0)
+    bpy.context.window_manager.pewpew_temporaryproperties.editmode_vertex_color = (0.5, 0.5, 1.0, 0.0)
     currentobj.data.vertex_colors.new()
     bpy.ops.mesh.select_all(action='SELECT')
     bpy.ops.mesh.set_vertex_colors()
