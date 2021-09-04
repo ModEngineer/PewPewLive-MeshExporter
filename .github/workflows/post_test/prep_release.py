390f72a6fb2490caf69bc79791e41edbf91937a2
ModEngineer <43882205+ModEngineer@users.noreply.github.com>
2021-09-04 11:35:37 -0500

    Update prep_release.py

--- .github/workflows/post_test/prep_release.py
+++ .github/workflows/post_test/prep_release.py
@@ -1,10 +1,7 @@
-import os, sys, shutil
-
-sys.path.append(os.path.join(os.environ["GITHUB_WORKSPACE"], "tempmodulefolder"))
-
-from ppl_meshexport_addon import bl_info
+import os, shutil, re
 
 with open(os.path.join(os.environ["GITHUB_WORKSPACE"], ".github", "workflows", "post_test", "version.txt"), "w", encoding="utf-8") as versionfile:
-    versionfile.write(".".join([ str(item) for item in bl_info["version"]]))
+    with open(os.path.join(os.environ["GITHUB_WORKSPACE"], "ppl_meshexport_addon", "__init__.py"), "r") as initpyhandle:
+        versionfile.write(".".join(re.search("\"version\": \((.*)\),", initpyhandle.read()).group(1).split(", ")))
 
 shutil.make_archive(os.path.join(os.environ["GITHUB_WORKSPACE"], ".github", "workflows", "post_test", "ppl_meshexport_addon"), "zip", os.environ["GITHUB_WORKSPACE"], "ppl_meshexport_addon")
