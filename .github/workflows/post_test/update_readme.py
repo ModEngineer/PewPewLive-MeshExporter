8017c94ebfdf860ec28a85bc8afd4fed5bd7f043
ModEngineer <43882205+ModEngineer@users.noreply.github.com>
2021-09-05 07:57:12 -0500

    Update update_readme.py

--- .github/workflows/post_test/update_readme.py
+++ .github/workflows/post_test/update_readme.py
@@ -1,1 +1,12 @@
+import os, re
 
+with open(os.path.join(os.environ["GITHUB_WORKSPACE"], "README.MD"), "r+", encoding="utf-8") as readmehandle:
+    testResultSection = ""
+    with open(os.path.join(os.environ["GITHUB_WORKSPACE"], ".github", "workflows", "post_test", "results_table.txt"), "r") as tablehandle1:
+        testResultSection+=tablehandle1.read()
+    with open(os.path.join(os.environ["GITHUB_WORKSPACE"], ".github", "workflows", "post_test", "traceback_table.txt")) as tablehandle2:
+        testResultSection+="\n\nAny errors produced during tests will be displayed below:\n" + tablehandle2.read()
+    readme = re.compile("(<!--tablestart-->)(.*)(<!--tableend-->)", re.DOTALL).sub("\\1" + testResultSection + "\\3", readmehandle.read())
+    readmehandle.seek(0, 0)
+    readmehandle.write(readme)
+    readmehandle.truncate()
