218d61e4a3273170eb1961304890ae891847e5ba
ModEngineer <43882205+ModEngineer@users.noreply.github.com>
2021-09-20 11:26:21 -0500

    Update blender_tests.py

--- .github/workflows/blender_tests/blender_tests.py
+++ .github/workflows/blender_tests/blender_tests.py
@@ -44,10 +44,7 @@ try:
         bpy.context.view_layer.objects.active = currentobj
     else:
         currentobj.select = True
-        if bpy.app.version > (2, 79, 0):
-            bpy.context.object = currentobj
-        else:
-            bpy.context.scene.object.active = currentobj
+        bpy.context.scene.object.active = currentobj
     bm = bmesh.new()
     bm.from_mesh(currentobj.data)
     seamVertexCoords = ([1, 1, 1], [-1, 1, 1], [-1, -1, 1])
