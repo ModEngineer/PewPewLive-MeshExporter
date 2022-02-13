99171c81e7711c3d1d61884e50377ab36e4818af
ModEngineer <43882205+ModEngineer@users.noreply.github.com>
2022-02-13 08:57:00 -0600

    Version bump

--- ppl_meshexport_addon/__init__.py
+++ ppl_meshexport_addon/__init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "author": "ModEngineer",
     "blender": (2, 80, 0),
     "description": "A mesh exporter for PewPew Live",
-    "version": (0, 5, 2),
+    "version": (0, 5, 3),
     "tracker_url":
     "https://www.github.com/ModEngineer/PewPewLive-MeshExporter/issues",
     "category": "Import-Export"
