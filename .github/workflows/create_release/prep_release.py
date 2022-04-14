bba1520151c3606617c60ccf3c5115ea74c49141
ModEngineer <43882205+ModEngineer@users.noreply.github.com>
2022-04-14 09:16:53 -0500

    Improved zipping

--- .github/workflows/create_release/prep_release.py
+++ .github/workflows/create_release/prep_release.py
@@ -1,4 +1,4 @@
-import os, shutil, ast
+import os, ast, zipfile
 
 with open(os.path.join(os.environ["GITHUB_WORKSPACE"], ".github", "workflows",
                        "create_release", "version.txt"),
@@ -14,7 +14,8 @@ with open(os.path.join(os.environ["GITHUB_WORKSPACE"], ".github", "workflows",
                 ast.literal_eval(ast.parse(
                     initpyhandle.read()).body[0].value)["version"])))
 
-shutil.make_archive(
-    os.path.join(os.environ["GITHUB_WORKSPACE"], ".github", "workflows",
-                 "create_release", "ppl_meshexport_addon"), "zip",
-    os.environ["GITHUB_WORKSPACE"], "ppl_meshexport_addon")
+with zipfile.ZipFile(os.path.join(os.environ["GITHUB_WORKSPACE"], ".github", "workflows", "create_release", "ppl_meshexport_addon.zip"), mode="x", compression=zipfile.ZIP_DEFLATED, compresslevel=9, allowZip64=False) as zipfilehandle:
+    for root, dirs, files in os.walk(os.path.join(os.environ["GITHUB_WORKSPACE"], "ppl_meshexport_addon")):
+        for file in files:
+            if file.endswith(".py") or any(substring in file.lower() for substring in ["license", "licence", "copying"]):
+                zipfilehandle.write(os.path.join(root, file), arcname=os.path.relpath(os.path.join(root, file), os.environ["GITHUB_WORKSPACE"]))
